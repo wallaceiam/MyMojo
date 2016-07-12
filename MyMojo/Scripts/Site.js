bdf48430386bc677c9eabb26418da41b332501d1
Chris <Chris@CHRIS-LAPTOP>
2016-07-12 19:59:13 +0100

    Added Scroll Up Button Added Google Analitics Added Roles Added Skills Formatting and Styling

--- MyMojo/Scripts/Site.js
+++ MyMojo/Scripts/Site.js
@@ -13,4 +13,37 @@
     //  Go! div wow
     wow.init();
 
+
+    /*======= Skillset *=======*/
+
+
+    $('.level-bar-inner').css('width', '0');
+
+    $(window).on('load', function () {
+
+        $('.level-bar-inner').each(function () {
+
+            var itemWidth = $(this).data('level');
+
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+
+        });
+
+    });
+
+    // ===== Scroll to Top ==== 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function () {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0                       // Scroll to top of body
+        }, 500);
+    });
 });
